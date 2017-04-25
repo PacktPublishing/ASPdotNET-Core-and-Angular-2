5746a6be65da76ef18131a779d639285cc2a1fcf
Darkseal <games@myst.it>
2017-04-25 17:24:01 +0200

    VS2017 Branch Initial Commit
    This is the initial commit for the VS2017 branch.
    There are no code changes, just the one-way upgrade of the Solution and
    Project files.
    
    IMPORTANT: If you're using Visual Studio 2017 you should use this branch
    from now on.

---
+++ src/OpenGameListWebApp_Chapter01/Scripts/app/app.module.js
@@ -0,0 +1,55 @@
+System.register(["@angular/core", "@angular/platform-browser", "@angular/http", "rxjs/Rx", "./app.component"], function (exports_1, context_1) {
+    "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __moduleName = context_1 && context_1.id;
+    var core_1, platform_browser_1, http_1, app_component_1, AppModule;
+    return {
+        setters: [
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (platform_browser_1_1) {
+                platform_browser_1 = platform_browser_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (_1) {
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            }
+        ],
+        execute: function () {
+            AppModule = (function () {
+                function AppModule() {
+                }
+                return AppModule;
+            }());
+            AppModule = __decorate([
+                core_1.NgModule({
+                    // directives, components, and pipes
+                    declarations: [
+                        app_component_1.AppComponent
+                    ],
+                    // modules
+                    imports: [
+                        platform_browser_1.BrowserModule,
+                        http_1.HttpModule
+                    ],
+                    // providers
+                    providers: [],
+                    bootstrap: [
+                        app_component_1.AppComponent
+                    ]
+                })
+            ], AppModule);
+            exports_1("AppModule", AppModule);
+        }
+    };
+});
